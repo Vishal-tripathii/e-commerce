2418d79946ff0014443e36fbfd3a8d49bdb5f000
Vishal <rdshrma61@gmail.com>
2023-10-15 17:04:46 +0530

    -implemented user login-logout with footer

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, OnInit } from '@angular/core';
-import { SignUp } from '../data-type';
+import { EventEmitter, Injectable, OnInit } from '@angular/core';
+import { LogIn, SignUp } from '../data-type';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { JsonPipe } from '@angular/common';
@@ -8,17 +8,39 @@ import { JsonPipe } from '@angular/common';
 })
 export class UserService implements OnInit {
   constructor(private http: HttpClient, private router: Router) { }
+  isLoginError = new EventEmitter<boolean>(false);
+
   ngOnInit(): void {
   }
   userSignUp(user: SignUp) {
-    console.warn(user, "servic file called");
-     this.http.post("http://localhost:3000/users", user, {observe:'response'}).subscribe((result) => {
-      console.log(result, "result from servic");
+    console.warn(user);
+    this.http.post("http://localhost:3000/users", user, {observe: 'response'}).subscribe((result) => {
+      console.log(result);
       if(result) {
-        localStorage.setItem('user', JSON.stringify(result));
+        localStorage.setItem('user', JSON.stringify(result.body));
+        this.router.navigate(['/'])
+      }
+      
+    })
+    
+  }
+  userLogin(data: LogIn) {
+    this.http.get<LogIn[]>(`http://localhost:3000/users?email=${data.email}&password=${data.password}`, {observe: 'response'}).subscribe((result) => {
+      if(result && result.body && result.body.length) { // here remember, the result mighht have a body with 0 length, so make sure u put the third condition as well so that only registered user may login
+        localStorage.setItem('user', JSON.stringify(result.body[0])); // here body could be null so we add to the if condition for body
         this.router.navigate(['/']);
+        this.isLoginError.emit(false)
+      }
+      else {
+        console.log("invalid-User");
+        this.isLoginError.emit(true);
       }
     })
   }
 
+  userAuthReload() {
+    if(localStorage.getItem('user')) {
+      this.router.navigate(['/'])
+    }
+  }
 }
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SignUp } from '../data-type';
+import { LogIn, SignUp } from '../data-type';
 import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-user-auth',
@@ -7,15 +7,27 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-auth.component.scss']
 })
 export class UserAuthComponent implements OnInit {
-
+  showLogin: boolean = true;
+  authError: string = '';
   constructor(private user_srvc: UserService) {}
   ngOnInit(): void {
+    this.user_srvc.userAuthReload();
   }
   signUp(data: SignUp) {
-    this.user_srvc.userSignUp(data);
-      // console.log(data, "resultData");
-      
-
+    this.user_srvc.userSignUp(data);     
+  }
+  login(data: LogIn) {
+    console.warn(data, "data");
+    this.user_srvc.userLogin(data); // calling the API 
+    this.user_srvc.isLoginError.subscribe((isError) => {
+      if(isError) {
+        this.authError = 'Incorrect email or password'
+      }
+    })
+    
+  }
+  toggleLogin() {
+    this.showLogin = !this.showLogin;
   }
 
 }
